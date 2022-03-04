956fe463d930b88d11cbb235a70b16c316bcb2ac
Chuks Ugwuh <chuks@ncktech.com>
2022-03-04 13:48:52 +0100

    fees adjusted

--- src/utils/getFeesAmount.js
+++ src/utils/getFeesAmount.js
@@ -2,16 +2,16 @@ export const getfeesAmount = (amount) => {
   let feesamount = '';
 
   if (amount >= 25000 && amount <= 50000) {
-    feesamount = 3000;
+    feesamount = 3500;
     return feesamount;
   } else if (amount >= 50000 && amount <= 100000) {
-    feesamount = 6000;
+    feesamount = 6500;
     return feesamount;
   } else if (amount > 100000 && amount <= 200000) {
-    feesamount = 10000;
+    feesamount = 10500;
     return feesamount;
   } else if (amount >= 250000 && amount <= 500000) {
-    feesamount = 20000;
+    feesamount = 21000;
     return feesamount;
   } else return 0;
 };
